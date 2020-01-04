72d0f02208df10013119ab66946d94c6711f79f3
Marco Salcedo <marcosalcedo@Marcos-MacBook-Pro.local>
2020-01-03 16:59:18 -0800

    adds fighters

---
+++ scripts/celebrities.js
@@ -0,0 +1,94 @@
+let fred = {
+    id: 0
+    name: "Fred Durst",
+    hp: 100,
+    attacks: [
+        {
+            attack: "Smash the Nookie",
+            damage: 90,
+            accuracy: .1,
+        },
+        {
+            attack: "Sucker Punch",
+            damage: 30,
+            accuracy: .5,
+        },
+        {
+            attack: "Microphone Smack",
+            damage: 10,
+            accuracy: 1,
+        }
+    ],
+    weakness: "Boy Bands",
+}
+
+let nick = {
+    id: 1
+    name: "Nick BSB",
+    hp: 100,
+    attacks: [
+        {
+            attack: "Backstreet Smack!",
+            damage: 90,
+            accuracy: .1,
+        },
+        {
+            attack: "Rock your body right hook",
+            damage: 30,
+            accuracy: .5,
+        },
+        {
+            attack: "Delicate punch",
+            damage: 10,
+            accuracy: 1,
+        }
+    ],
+    weakness: "Queen of Pop",
+}
+let fred = {
+    id: 2
+    name: "Britney",
+    hp: 100,
+    attacks: [
+        {
+            attack: "Killing Loneliness",
+            damage: 90,
+            accuracy: .1,
+        },
+        {
+            attack: "Toxic",
+            damage: 30,
+            accuracy: .5,
+        },
+        {
+            attack: "Baby one more punch",
+            damage: 10,
+            accuracy: 1,
+        }
+    ],
+    weakness: "Satirical Rap",
+}
+let fred = {
+    id: 3
+    name: "Eminem",
+    hp: 100,
+    attacks: [
+        {
+            attack: "Chance to blow",
+            damage: 90,
+            accuracy: .1,
+        },
+        {
+            attack: "Shady Liver Chop",
+            damage: 30,
+            accuracy: .5,
+        },
+        {
+            attack: "Sweaty Palms",
+            damage: 10,
+            accuracy: 1,
+        }
+    ],
+    weakness: "Lame Rap-Rock",
+}
+
