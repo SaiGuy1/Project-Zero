69cea9021432739527e5a63a32f3e63c9d8f6db5
Marco Salcedo <marcosalcedo@Marcos-MacBook-Pro.local>
2020-01-07 19:35:29 -0800

    adds attack mechanic in the console

--- scripts/app.js
+++ scripts/app.js
@@ -140,7 +140,7 @@ function createFighters(){
             <div class="card-header">
                 <h1 class="my-0 font-weight-normal h1_fonts">${fighter.name}</h1>
             </div>
-            <div class="card-body p-1 d-flex flex-column justify-content-center ">
+            <div class="card-body p-1 d-flex flex-column justify-content-center">
                 <img src=${fighter.image} class="w-25 align-self-center rounded-circle" alt="${fighter.name}">
                 <ul class="list-unstyled">
                     <li "font-size: 10px;"><b>${fighter.quote}</b></li>
@@ -178,11 +178,12 @@ function createFighters(){
 
 //This function listens to who the user selected to create the battle-ring and switch the character card to the fighting card, which has less information but more functionality.
 function battleStart (event) {
+    let leftOvers = [];
     $display.append(`
     <div id="battle-scene" class="d-flex flex-column justify-content-center align-items-center h-100 w-75">
         <div id="referee" class="d-flex flex-row h-25 w-75">
         </div>
-        <div id="fight-ring" class="card-deck d-flex flex-row h-75 w-75">
+        <div id="fight-ring" class="card-deck d-flex flex-row justify-content-around h-75 w-75">
         </div>
     </div>
     `)
@@ -190,14 +191,14 @@ function battleStart (event) {
     fighters.forEach(fighter => {
         if(fighter.id===$(event.target).parent().attr('id')){
             $('#fight-ring').append(`
-            <div id="${fighter.id}" class="card shadow-sm">
-                <div class="card-header">
-                    <h1 class="my-0 font-weight-normal h1_fonts">${fighter.name}</h1>
+            <div id="${fighter.id}" class="card shadow-sm ring-card m-0">
+                <div class="card-header d-flex justify-content-center align-items-center">
+                    <h1 class="h1_fonts">${fighter.name}</h1>
                 </div>
                 <div class="card-body p-1 d-flex flex-column justify-content-center ">
                     <img src=${fighter.image} class="w-25 align-self-center rounded-circle" alt="${fighter.name}">
                     <div class="progress">
-                        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: 100%" aria-valuenow="${fighter.hp}" aria-valuemin="0" aria-valuemax="${fighter.hp}">${fighter.hp}HP</div>
+                        <div class="bg-success progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 100%" aria-valuenow="${fighter.hp}" aria-valuemin="0" aria-valuemax="${fighter.hp}">${fighter.hp}HP</div>
                     </div>
                 </div>
                 <div id="${fighter.name}-attacks">
@@ -206,17 +207,49 @@ function battleStart (event) {
             `);
             fighter.attacks.forEach(attack => {
                 $(`#${fighter.name}-attacks`).append(`
-                <button type="button" class="btn btn-dark btn-block btn-outline-red" pow="${attack.damage}" acc="${attack.accuracy}">Name: ${attack.attack}<br>POW: ${attack.damage} ACC: ${attack.accuracy*100}%</button>
+                <button type="button" class="btn-dark btn-block btn-outline-red attack-button p-1 m-1" pow="${attack.damage}" acc="${attack.accuracy}">ATK: ${attack.attack}<br>POW: ${attack.damage} ACC: ${attack.accuracy*100}%</button>
                 `);
             });
-
+        } else {
+            leftOvers.push(fighter)
         }
     });
+    //console.log(leftOvers[Math.floor(Math.random()*leftOvers.length)]);
+    let randomFighter = leftOvers[Math.floor(Math.random()*leftOvers.length)];
+    //console.log(randomFighter);
+    {$('#fight-ring').append(`
+            <div id="${randomFighter.id}" class="card shadow-sm ring-card m-0">
+                <div class="card-header d-flex justify-content-center align-items-center">
+                    <h1 class="h1_fonts">${randomFighter.name}</h1>
+                </div>
+                <div class="card-body p-1 d-flex flex-column justify-content-center">
+                    <img src=${randomFighter.image} class="w-25 align-self-center rounded-circle" alt="${randomFighter.name}">
+                    <div class="progress">
+                        <div class="bg-success progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 100%" aria-valuenow="${randomFighter.hp}" aria-valuemin="0" aria-valuemax="${randomFighter.hp}">${randomFighter.hp}HP</div>
+                    </div>
+                </div>
+                <div id="${randomFighter.name}-attacks">
+                </div>
+            </div>
+            `);}
+            randomFighter.attacks.forEach(attack => {
+                $(`#${randomFighter.name}-attacks`).append(`
+                <button type="button" class="btn-dark btn-block btn-outline-red attack-button p-1 m-1" pow="${attack.damage}" acc="${attack.accuracy}">ATK: ${attack.attack}<br>POW: ${attack.damage} ACC: ${attack.accuracy*100}%</button>
+                `);
+            });
     $('#character-selector').remove();
 }
 
+function fightMove (event) {
+    if($(event.target).attr('acc')>Math.random()){
+        console.log(`You dealt ${$(event.target).attr('pow')} damage!`)
+    } else {
+        console.log('You missed!')
+    }
+}
 // ***************************** Event Listeners **************************************************************
 $('#game-start').on('click', gameStart);
 $display.on('click', '#game-mode-selected', createFighters);
 $display.on('click', '.character-selected', battleStart);
+$display.on('click', '.attack-button', fightMove);
 
